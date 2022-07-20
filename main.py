361445c1de1b64432c6d5aa73ae19c0193c08b7b
AlexAndHisScripts <alexcj2802@gmail.com>
2022-07-20 15:32:48 +0100

    Object tracking

--- main.py
+++ main.py
@@ -5,13 +5,37 @@ cap = cv.VideoCapture(0)
 
 
 class Parameters():
-    min = 100
+    min = 90
     max = 200
     aperture = 3
 
 
+tracker = cv.TrackerCSRT_create()
+bbox = (0, 0, 50, 50)
+first = True
+
 while True:
     ret, frame = cap.read()
+
+    frame = cv.Canny(frame, Parameters.min, Parameters.max, apertureSize=Parameters.aperture)
+
+    frame = cv.resize(frame, (frame.shape[1] * 2, frame.shape[0] * 2), 2, 2)
+
+    if first:
+        bbox = cv.selectROI(frame, False)
+        ok = tracker.init(frame, bbox)
+
+    first = False
+
+    print(bbox)
+
+    ok, bbox = tracker.update(frame)
+
+    frame = cv.cvtColor(frame, cv.COLOR_GRAY2RGB)
+
+    p1 = (int(bbox[0]), int(bbox[1]))
+    p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+    cv.rectangle(frame, p1, p2, (0, 0, 255), 2, 1)
 
     if keyboard.is_pressed("7"):
         Parameters.min = Parameters.min + 1
@@ -36,10 +60,6 @@ while True:
             Parameters.aperture = Parameters.aperture - 2
 
     print(Parameters.min, Parameters.max, Parameters.aperture)
-
-    frame = cv.Canny(frame, Parameters.min, Parameters.max, apertureSize=Parameters.aperture)
-
-    frame = cv.resize(frame, (frame.shape[1] * 2, frame.shape[0] * 2), 2, 2)
 
     cv.imshow("Input", frame)
 
