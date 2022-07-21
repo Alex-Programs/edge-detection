eeb74db98dc8a0b73b99fb54d500f8ab87256d56
AlexAndHisScripts <alexcj2802@gmail.com>
2022-07-21 09:41:00 +0100

    Fancy displays

--- main.py
+++ main.py
@@ -5,14 +5,10 @@ cap = cv.VideoCapture(0)
 
 
 class Parameters():
-    min = 90
-    max = 200
+    min = 70
+    max = 150
     aperture = 3
 
-
-tracker = cv.TrackerCSRT_create()
-bbox = (0, 0, 50, 50)
-first = True
 
 while True:
     ret, frame = cap.read()
@@ -21,21 +17,45 @@ while True:
 
     frame = cv.resize(frame, (frame.shape[1] * 2, frame.shape[0] * 2), 2, 2)
 
-    if first:
-        bbox = cv.selectROI(frame, False)
-        ok = tracker.init(frame, bbox)
+    contours, heirarchy = cv.findContours(frame, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-    first = False
+    simplified = []
 
-    print(bbox)
+    for polygon in contours:
+        simplified.append(cv.approxPolyDP(polygon, 0.01 * cv.arcLength(polygon, True), True))
 
-    ok, bbox = tracker.update(frame)
+    simplified = contours
 
     frame = cv.cvtColor(frame, cv.COLOR_GRAY2RGB)
 
-    p1 = (int(bbox[0]), int(bbox[1]))
-    p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-    cv.rectangle(frame, p1, p2, (0, 0, 255), 2, 1)
+    largestContourSize = 0
+    largestContour = None
+    largestContourIndex = 0
+    index = 0
+
+    for polygon in simplified:
+        index += 1
+
+        area = cv.contourArea(polygon)
+        if area > largestContourSize:
+            largestContourSize = area
+            largestContour = polygon
+            largestContourIndex = index
+
+    # overlay the caclulated polygons with a white area and red border. If it's the largest contour, fill in blue with green border
+    index = 0
+
+    for polygon in simplified:
+        index += 1
+
+        if index == largestContourIndex:
+            cv.drawContours(frame, [polygon], -1, (255, 0, 0), -1)
+            cv.drawContours(frame, [polygon], -1, (0, 255, 0), 1)
+            continue
+
+        continue
+        cv.drawContours(frame, [polygon], -1, (255, 255, 255), -1)
+        cv.drawContours(frame, [polygon], -1, (0, 0, 255), 1)
 
     if keyboard.is_pressed("7"):
         Parameters.min = Parameters.min + 1
